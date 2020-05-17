c91c9e722cf790a9eb7d0b6b90f9333b2063c0ac
Morten Voigt Jagd <mort237d@edu.easj.dk>
2020-05-17 16:18:37 +0200

    Screen works

---
+++ app/src/main/java/com/example/homelock/LCD1602.java
@@ -0,0 +1,41 @@
+package com.example.homelock;
+
+import com.nilhcem.androidthings.driver.lcdpcf8574.LcdPcf8574;
+
+import java.io.IOException;
+
+public class LCD1602 {
+    private static final String TAG = "LCD1602";
+    private LcdPcf8574 lcd;
+
+    public LCD1602() {
+        try {
+            lcd = new LcdPcf8574("I2C1", 0x3f);
+            lcd.begin(16, 2);
+            lcd.setBacklight(true);
+
+            int[] heart = {0b00000, 0b01010, 0b11111, 0b11111, 0b11111, 0b01110, 0b00100, 0b00000};
+            lcd.createChar(0, heart);
+
+            int[] arrow = {0b00100, 0b01110, 0b11111, 0b00100, 0b00100, 0b00100, 0b00100, 0b00000};
+            lcd.createChar(1, arrow);
+
+            int[] android = {0b00100, 0b01110, 0b11111, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111};
+            lcd.createChar(2, android);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Print(String text1, String text2){
+        try {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print(text1);
+            lcd.setCursor(0, 1);
+            lcd.print(text2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/homelock/MainActivity.java
+++ app/src/main/java/com/example/homelock/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends Activity {
     private ServoMotor servoMotor;
     private LED greenLED;
     private LED redLED;
-    private  PhysicalButton greenButton;
-    private  PhysicalButton redButton;
+    private PhysicalButton greenButton;
+    private PhysicalButton redButton;
+    private LCD1602 screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,9 @@ public class MainActivity extends Activity {
         greenLED = new LED(pm, "BCM20");
         redButton = new PhysicalButton(pm, "BCM19");
         greenButton = new PhysicalButton(pm, "BCM26");
+        screen = new LCD1602();
+
+        screen.Print("HomeLock", "");
 
         while(true){
             if (greenButton.Pushed()) Unlock();
@@ -36,11 +40,13 @@ public class MainActivity extends Activity {
         servoMotor.Swing90Degrees();
         greenLED.turnOff();
         redLED.turnOn();
+        screen.Print("Locked", "");
     }
 
     private void Unlock(){
         servoMotor.Swing0Degrees();
         redLED.turnOff();
         greenLED.turnOn();
+        screen.Print("Unlocked", "");
     }
 }
