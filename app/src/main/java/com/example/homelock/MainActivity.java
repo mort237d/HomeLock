89b95d82fca30378dd92b2dc5434c7461d71917e
Morten Voigt Jagd <mort237d@edu.easj.dk>
2020-05-18 12:21:32 +0200

    timer set back to zero

--- app/src/main/java/com/example/homelock/MainActivity.java
+++ app/src/main/java/com/example/homelock/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends Activity {
             try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
             timer += 1;
         }
+        timer = 0;
         screen.Print("Bluetooth", "No devices attached");
     }
 
