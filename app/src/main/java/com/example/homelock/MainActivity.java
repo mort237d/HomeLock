c3975443fd905aa811da55d39e159cd26cdee55f
Morten Voigt Jagd <mort237d@edu.easj.dk>
2020-05-18 08:47:03 +0200

    Pairing LED

--- app/src/main/java/com/example/homelock/MainActivity.java
+++ app/src/main/java/com/example/homelock/MainActivity.java
@@ -10,8 +10,10 @@ public class MainActivity extends Activity {
     private ServoMotor servoMotor;
     private LED greenLED;
     private LED redLED;
+    private LED blueLED;
     private PhysicalButton greenButton;
     private PhysicalButton redButton;
+    private PhysicalButton blueButton;
     private LCD1602 screen;
 
     @Override
@@ -24,8 +26,10 @@ public class MainActivity extends Activity {
         servoMotor = new ServoMotor(pm);
         redLED = new LED(pm, "BCM21");
         greenLED = new LED(pm, "BCM20");
+        blueLED = new LED(pm, "BCM12");
         redButton = new PhysicalButton(pm, "BCM19");
         greenButton = new PhysicalButton(pm, "BCM26");
+        blueButton = new PhysicalButton(pm, "BCM6");
         screen = new LCD1602();
 
         screen.Print("HomeLock", "");
@@ -33,6 +37,27 @@ public class MainActivity extends Activity {
         while(true){
             if (greenButton.Pushed()) Unlock();
             if (redButton.Pushed()) Lock();
+            if (blueButton.Pushed()) Pairing();
+        }
+    }
+
+    private void Pairing() {
+        int timer = 0;
+        int maxTime = 10;
+
+        screen.Print("Pairing", "Waiting for device");
+        while (timer < maxTime){
+            try {
+                blueLED.turnOn();
+                Thread.sleep(500);
+                blueLED.turnOff();
+                Thread.sleep(500);
+                timer += 1;
+
+                if (blueButton.Pushed()) timer = maxTime;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
